3709ff33de173dd481525a4fca9bf1fac0854d7b
mr_aboda <abdelbasetmohamed228@gmail.com>
2024-03-15 02:37:27 +0200

    hi1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_calculator/calculator_cubit/cubit.dart';
+import 'package:new_calculator/calculator_cubit/state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,44 +32,36 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({super.key, required this.title});
 
   final String title;
- @override
+  @override
   Widget build(BuildContext context) {
-    @override
-  Widget BlocConsumer<CounterCubit, CounterState>(
-    listener: (context, state){}
-    builder: (context, state) {
-      return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'the counter presents',
-            ),
-            
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(onPressed: () {}, icon: Icon(Icons.plus_one)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.minimize))
+    return BlocConsumer<CounterCubit, CounterState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'the counter presents',
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.plus_one)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.minimize))
+                  ],
+                )
               ],
-            )
-          ],
-        ),
-      ),
+            ),
+          ),
+        );
+      },
     );
-      
-    },
-  ) {
-    
-  }
   }
 }
-  
-
-
