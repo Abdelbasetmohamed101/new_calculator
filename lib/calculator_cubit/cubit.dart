e6056e88b2a05e4d960a69dd9c2665c6e8a9a45f
mr_aboda <abdelbasetmohamed228@gmail.com>
2024-03-16 12:35:26 +0200

    last change

--- lib/calculator_cubit/cubit.dart
+++ lib/calculator_cubit/cubit.dart
@@ -10,23 +10,26 @@ class CalculatorCubit extends Cubit<CalculatorState> {
   int num_2 = 0;
   String op = '';
   void Value_Check(value) {
-    if (op == '' && value! is String) {
+    if (op == '' && value is! String) {
       num_1 = value;
+      this.value = value;
       emit(NumberOneState());
-    } else if (value is String) {
+    } else if (value is String && value != 'AC') {
       op = value;
+      this.value = value;
       emit(OperatorState());
-    } else if (op != '' && value! is String) {
+    } else if (op != '' && value is! String) {
       num_2 = value;
+      this.value = value;
       emit(NumberTwoState());
     } else if (value == "AC") {
       op = '';
       num_1 = 0;
       num_2 = 0;
-      value = 0;
+      this.value = 0;
       emit(ClearState());
     } else {
-      value = "error";
+      this.value = "error";
       emit(ErrorState());
     }
   }
@@ -50,7 +53,7 @@ class CalculatorCubit extends Cubit<CalculatorState> {
         break;
       case '/':
         if (num_2 == 0) {
-          value = "error";
+          this.value = "error";
           emit(ErrorState());
         } else {
           result = num_1 / num_2;
